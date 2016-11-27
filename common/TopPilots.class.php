405f5eee17e84685c5d6fa216146eddfb5dc9921
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-11-27 20:14:24 +0330

    Version Check Is Needed

--- common/TopPilots.class.php
+++ common/TopPilots.class.php
@@ -6,10 +6,10 @@ class TopPilots extends CodonData
 			$dbm="SELECT t1.pilotid,t1.flighttime,t2.firstname,t2.lastname,t2.code,t2.rank FROM 
 					(SELECT 
 						pilotid, sum(flighttime) as flighttime 
-							FROM phpvms_pireps 
+							FROM ".TABLE_PREFIX."pireps 
 								WHERE date_format(submitdate, '%Y') = date_format(now(), '%Y') AND accepted = 1
 									GROUP BY pilotid) t1
-										LEFT JOIN phpvms_pilots t2 ON t1.pilotid = t2.pilotid 
+										LEFT JOIN ".TABLE_PREFIX."pilots t2 ON t1.pilotid = t2.pilotid 
 											ORDER BY flighttime DESC LIMIT 10";
 											return DB::get_results($dbm);
 		}
@@ -19,10 +19,10 @@ class TopPilots extends CodonData
 			$dbm="SELECT t1.pilotid,t1.distance,t2.firstname,t2.lastname,t2.code,t2.rank FROM 
 					(SELECT 
 						pilotid, sum(distance) as distance 
-							FROM phpvms_pireps 
+							FROM ".TABLE_PREFIX."pireps 
 								WHERE date_format(submitdate, '%Y') = date_format(now(), '%Y') AND accepted = 1
 									GROUP BY pilotid) t1
-										LEFT JOIN phpvms_pilots t2 ON t1.pilotid = t2.pilotid 
+										LEFT JOIN ".TABLE_PREFIX."pilots t2 ON t1.pilotid = t2.pilotid 
 											ORDER BY distance DESC LIMIT 10";
 											return = DB::get_results($dbm);
 		}
@@ -32,10 +32,10 @@ class TopPilots extends CodonData
 			$dbm="SELECT t1.pilotid,t1.pirepid,t1.revenue,t2.firstname,t2.lastname,t2.code,t2.rank FROM 
 					(SELECT 
 						pilotid, count(pirepid) as pirepid, sum(revenue) as revenue 
-							FROM phpvms_pireps 
+							FROM ".TABLE_PREFIX."pireps 
 								WHERE date_format(submitdate, '%Y') = date_format(now(), '%Y') AND accepted = 1
 									GROUP BY pilotid) t1
-										LEFT JOIN phpvms_pilots t2 ON t1.pilotid = t2.pilotid 
+										LEFT JOIN ".TABLE_PREFIX."pilots t2 ON t1.pilotid = t2.pilotid 
 											ORDER BY revenue DESC LIMIT 10";
 											return DB::get_results($dbm);
 		}
--- modules/TopPilot/TopPilot.php
+++ modules/TopPilot/TopPilot.php
@@ -3,9 +3,16 @@ class TopPilot extends CodonModule
 {
 	public function index() 
 		{
-           
+			$revision = trim(file_get_contents(CORE_PATH.'/version'));
+			if($revision != 'simpilot 5.5.2')
+				{
+					echo '<center>phpVMS Version Installed Is Not Compatible With This Module!</center><br />';
+					echo '<center>phpVMS Version Installed: '.$revision.'</center>';
+				}
+			else
+			{
             $this->show('/toppilot/toppilot.php');
-			            
+			}           
         }
 }
 ?>
